149b64594f3f631c785e490b438e860d5526d5e1
madalina.andronache <madi_andronache2011@yahoo.com>
2025-05-15 19:17:56 +0300

    Added basic testing for LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,32 @@
-// +---------------------------------------------------------------------------+
-// |                             PM/MA lab skel                                |
-// +---------------------------------------------------------------------------+
-
-//! By default, this app prints a "Hello world" message with `defmt`.
-
 #![no_std]
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_net::StackResources;
-use embassy_time::{Duration, Timer};
-use static_cell::StaticCell;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::init;
+use embassy_time::{Timer, Duration};
+
 use {defmt_rtt as _, panic_probe as _};
 
-// Use the logging macros provided by defmt.
-use defmt::*;
-
-// Import interrupts definition module
-mod irqs;
-
-const SOCK: usize = 4;
-static RESOURCES: StaticCell<StackResources<SOCK>> = StaticCell::<StackResources<SOCK>>::new();
-
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    // Get a handle to the RP's peripherals.
-    let peripherals = embassy_rp::init(Default::default());
+async fn main(_spawner: Spawner) {
+    let p = init(Default::default());
 
-    // Init WiFi driver
-    let (net_device, mut _control) = embassy_lab_utils::init_wifi!(&spawner, peripherals).await;
+    let mut led1 = Output::new(p.PIN_18, Level::Low);
+    let mut led2 = Output::new(p.PIN_19, Level::Low);
+    let mut led3 = Output::new(p.PIN_20, Level::Low);
 
-    // Default config for dynamic IP address
-    let config = embassy_net::Config::dhcpv4(Default::default());
 
-    // Init network stack
-    let _stack = embassy_lab_utils::init_network_stack(&spawner, net_device, &RESOURCES, config);
-
-    info!("Hello world!");
-
-    let delay = Duration::from_secs(1);
     loop {
-        Timer::after(delay).await;
+        led1.set_high();
+        led2.set_high();
+        led3.set_high();
+        Timer::after(Duration::from_millis(500)).await;
+
+        led1.set_low();
+        led2.set_low();
+        led3.set_low();
+        Timer::after(Duration::from_millis(500)).await;
     }
+    
 }
