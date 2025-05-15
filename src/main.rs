0152d1720f8fe4a337e9d5860c0aa8028d4fa4b8
madalina.andronache <madi_andronache2011@yahoo.com>
2025-05-15 19:47:19 +0300

    Added buzzer control

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use embassy_time::{Timer, Duration};
 
 use {defmt_rtt as _, panic_probe as _};
 
+// Blink all 3 LEDs at the same time
 async fn blink_all(
     led1: &mut Output<'static>,
     led2: &mut Output<'static>,
@@ -24,17 +25,28 @@ async fn blink_all(
     Timer::after(Duration::from_millis(500)).await;
 }
 
+// Beep the buzzer
+async fn beep(buzzer: &mut Output<'static>) {
+    buzzer.set_high();
+    Timer::after(Duration::from_millis(200)).await;
+    buzzer.set_low();
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+    // Initialize the peripherals
     let p = init(Default::default());
 
+    // Initialize 3 LEDs 
     let mut led1 = Output::new(p.PIN_18, Level::Low);
     let mut led2 = Output::new(p.PIN_19, Level::Low);
     let mut led3 = Output::new(p.PIN_20, Level::Low);
 
+    // Initialize the buzzer
+    let mut buzzer = Output::new(p.PIN_16, Level::Low);
 
     loop {
         blink_all(&mut led1, &mut led2, &mut led3).await;
+        beep(&mut buzzer).await;
     }
 }
