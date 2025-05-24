c9875433d70126bf0b1ba0bec65f034f68ec7f1d
madalina.andronache <madi_andronache2011@yahoo.com>
2025-05-24 22:13:25 +0300

    Added SOS button

--- Source code/src/main.rs
+++ Source code/src/main.rs
@@ -106,7 +106,7 @@ fn init_keypad(
 
     let keys = [
         ['1', '2', '3', '!'],
-        ['4', '5', '6', 'B'],
+        ['4', '5', '6', '^'],
         ['7', '8', '9', ')'],
         ['*', '0', '#', '('],
     ];
@@ -283,6 +283,11 @@ async fn handle_multitap_input(
             *last_key = None;
             *tap_index = 0;
             return Some((')', false));
+        } else if key == '^' {
+            defmt::info!("SOS key pressed: '!'");
+            *last_key = None;
+            *tap_index = 0;
+            return Some(('^', false));
         } 
 
         if get_multitap_chars(key).is_none() {
@@ -525,7 +530,35 @@ async fn main(_spawner: Spawner) {
 
                 Timer::after(Duration::from_secs(2)).await;
                 continue;
+            } else if c == '^' {
+                // S.O.S
+                let message = "SOS";
+
+                lcd.clean_display();
+                lcd.set_cursor_pos((0, 0));
+                lcd.write_str_to_cur("Sending S.O.S");
+
+                for ch in message.chars() {
+                    lcd.set_cursor_pos((0, 1));
+                    lcd.write_str_to_cur("Char: ");
+                    lcd.write_char_to_cur(ch);
+
+                    if let Some(code) = morse_table(ch) {
+                        lcd.set_cursor_pos((0, 1));
+                        lcd.write_str_to_cur("Morse: ");
+                        lcd.write_str_to_cur(code);
+
+                        display_letter_morse(ch, &mut led1, &mut led2, &mut led3, &mut buzzer).await;
+                    }
+                }
+
+                lcd.clean_display();
+                lcd.set_cursor_pos((0, 0));
+                lcd.write_str_to_cur("S.O.S sent!");
+                Timer::after(Duration::from_secs(1)).await;
+                continue;
             }
+
 
             if let Some(code) = morse_table(c) {
                 lcd.clean_display();
